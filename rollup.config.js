e678adfa625bcaced29012d2ef50f0204b5a79c7
Michael Witham <michael.a.witham@outlook.com>
2022-03-22 22:32:58 -0500

    feat: add rollup and react

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [nodeResolve(), commonjs(), typescript()],
+};
